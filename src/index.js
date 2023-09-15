884b6bd9913631e6a87c0f25d07ae51f039d4b7c
Nikita <terencekity@gmail.com>
2023-09-15 05:09:31 -0400

    complete the lab and improve the code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector("#create-task-form")
+  const ul = document.querySelector('#tasks')
+  // const btn = document.createElement('button')
+  form.addEventListener('submit', submitFunc)
+  
+;
+function submitFunc (event){
+  event.preventDefault()
+  let comment = event.target
+  const btn = document.createElement('button')
+  btn.textContent = ' X '
+  comment = comment.querySelector('#new-task-description')
+  comment = comment.value
+  const p = document.createElement('p')
+  btn.addEventListener('click',(e)=>{
+    btn.parentNode.remove(btn)
+  })
+  if (comment){
+  p.textContent = comment
+  // console.log(comment)
+p.append(btn)
+  ul.append(p)
+  event.target.reset()}
+}
+
+})
